0d79d93a3df0ccbaea475011660d1d33f85ff447
ivan <ierodriguezgo@gmail.com>
2020-08-21 13:36:13 -0500

    Conection MongoDB Atlas

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const Pilot = require("./src/models/pilot.model");
-app.use(express.json());
 const db = require("./src/db");
 const pilotRouter = require("./src/routes/pilot")
 
-db();
-// app.use(cors());
+const port = 8000;
+
+//db();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/pilots", pilotRouter)
 
-app.listen(8000, () => console.log("App listening localhost:8000"));
+app.listen(port, () => 
+  console.log("App listening localhost:8000"));
--- src/routes/pilot.js
+++ src/routes/pilot.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const pilotController = require('../controllers/pilot.controller')
+const pilotControllerRemote = require('../controllers/pilot.controller.remote')
 
 
-router.route("/listar").get(pilotController.list)
+router.route("/listar").get(pilotControllerRemote.list)
 router.route("/crear").post(pilotController.create)
 router.route("/listar/:id").get(pilotController.show)
 router.route("/actualizar/:id").put(pilotController.update)
