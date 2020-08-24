2ee41097539098007730b97b9c3d3f6b9ef2be08
Luis Felipe Machado <luis.felipe.machado13@gmail.com>
2020-08-24 12:00:34 -0500

    fixes try in pilot controller methods

--- src/controllers/pilot.controller.js
+++ src/controllers/pilot.controller.js
@@ -7,37 +7,37 @@ const jwt = require('jsonwebtoken');
 module.exports = {
    
   async list_clients(req, res) {
-    const clients = await Client.find({});
-
     try {
+      const clients = await Client.find({});
       res.status(200).json(clients);
+
     } catch (err) {
       res.status(400).json(err);
     }
   },
 
   async list(req, res) {
-    const pilots = await Pilot.find({});
-
     try {
+      const pilots = await Pilot.find({});
       res.status(200).json(pilots);
+
     } catch (err) {
       res.status(400).json(err);
     }
   },
 
   async signup(req, res) {
-    const data = req.body;
-
     try {
+      const data = req.body;
       const encryptedPassword = await bcrypt.hash(data.password, 8);
+
       if (data.userType === "pilot") {
         const pilot = await Pilot.create({ ...data, password: encryptedPassword });
       } else if (data.userType === "client") {
         const client = await Client.create({ ...data, password: encryptedPassword });
       }
-
       res.status(200).json();
+
     } catch(err) {
       res.status(400).json(err);
     }
@@ -47,7 +47,6 @@ module.exports = {
   async signin(req, res) {
     try {
       const { email, password } = req.body;
-
       const pilot = await Pilot.findOne({ email });
 
       if(!pilot) {
@@ -66,46 +65,43 @@ module.exports = {
         { expiresIn: 60 * 60 * 24 * 365 }
       );
 
-      res.status(200).json({token});
+      res.status(200).json({token, pilot});
     } catch(err) {
       res.status(401).json({ message: err.message })
     }
   },
 
   async show(req, res) {
-    const { id } = req.params;
-    const pilot = await Pilot.findById(id);
-
     try {
+      const { id } = req.params;
+      const pilot = await Pilot.findById(id);
       res.status(200).json(pilot);
+
     } catch (err) {
       res.status(400).json({ message: `Could not find task with id ${id}` });
     }
   },
 
   async update(req, res) {
-    const { id } = req.params;
-    const data = req.body;
-
-    const pilot = await Pilot.findByIdAndUpdate(id, data, { new: true })
-
     try {
+      const { id } = req.params;
+      const data = req.body;
+      const pilot = await Pilot.findByIdAndUpdate(id, data, { new: true })
       res.status(200).json(pilot);
+
     } catch (err) {
       res.status(400).json(err);
     }
   },
 
   async destroy(req, res) {
-    const { id } = req.params;
-
-    const pilot = await Pilot.findByIdAndDelete(id)
-
     try {
+      const { id } = req.params;
+      const pilot = await Pilot.findByIdAndDelete(id)
       res.status(200).json(pilot);
+
     } catch (err) {
       res.status(400).json({ message: `Could not find task with id ${id}` });
     }
   }
-
 }
