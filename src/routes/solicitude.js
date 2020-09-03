49f91f8a1892e2fb39d2e94f2f0fdd238000d700
ivan <ierodriguezgo@gmail.com>
2020-09-03 14:15:25 -0500

    fix bug showpilot method in solicitude controller

--- src/routes/solicitude.js
+++ src/routes/solicitude.js
@@ -2,11 +2,11 @@ const router = require('express').Router()
 const solicitudeController = require('../controllers/solicitude.controller')
 
 
-/* router.route("/listar").get(solicitudeController.list) */
+router.route("/listar").get(solicitudeController.list)
 router.route("/crear").post(solicitudeController.create)
-/* router.route("/listar/:id").get(solicitudeController.show)
-router.route("/listar/piloto/:id").get(solicitudeController.showpilot)
+router.route("/listar/:id").get(solicitudeController.show)
+router.route("/listar/piloto/:id").get(solicitudeController.showclient)
 router.route("/actualizar/:id").put(solicitudeController.update)
-router.route("/eliminar/:id").delete(solicitudeController.destroy) */
+router.route("/eliminar/:id").delete(solicitudeController.destroy)
 
 module.exports = router
