2fd8e5a8b5caf57d2b68896d1b3e4018e52cd5d6
Jonathan Palacio <jpalacio0612@gmail.com>
2020-09-06 17:44:24 -0500

    Add pilot-explora backend

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const pilotRouter = require("./src/routes/pilot");
 const mediaRouter = require("./src/routes/media");
 const clientRouter = require("./src/routes/client");
 const solicitudeRouter = require("./src/routes/solicitude");
 const certificateRouter = require("./src/routes/certificate");
 const serviceRouter = require("./src/routes/service");
-const { auth } = require('./src/utils/middlewares')
+const { auth } = require("./src/utils/middlewares");
 
 const db = require("./src/db");
 
@@ -25,5 +25,4 @@ app.use("/solicitudes", solicitudeRouter);
 app.use("/certificados", certificateRouter);
 app.use("/servicios", serviceRouter);
 
-app.listen(port, () => 
-  console.log("App listening localhost:8000"));
+app.listen(port, () => console.log("App listening localhost:8000"));
--- src/routes/client.js
+++ src/routes/client.js
@@ -1,13 +1,13 @@
-const router = require('express').Router()
-const clientController = require('../controllers/client.controller')
-const { auth } = require('../utils/middlewares')
+const router = require("express").Router();
+const clientController = require("../controllers/client.controller");
+const { auth } = require("../utils/middlewares");
 
-router.route("/listar").get(clientController.list)
-router.route("/crear").post(clientController.signup)
-router.route("/login").post(clientController.signin)
-router.route("/listar/:id").get(clientController.findUser)
-router.route("/actualizar/:id").put(clientController.update)
-router.route("/eliminar/:id").delete(clientController.destroy)
-router.route("/filtrar").post(clientController.filter)
+router.route("/listar").get(clientController.list);
+router.route("/crear").post(clientController.signup);
+router.route("/login").post(clientController.signin);
+router.route("/listar/:id").get(clientController.findUser);
+router.route("/actualizar/:id").put(clientController.update);
+router.route("/eliminar/:id").delete(clientController.destroy);
+router.route("/filtrar").post(clientController.filter);
 
-module.exports = router
+module.exports = router;
