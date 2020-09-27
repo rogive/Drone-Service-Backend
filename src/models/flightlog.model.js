6c813796ae670c6b40a7e6c227a2bb61e9982ba6
ivan <ierodriguezgo@gmail.com>
2020-09-26 23:40:49 -0500

    flightlog MVC created

---
+++ src/models/flightlog.model.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose')
+
+const flightlogSchema = new Schema({ 
+  name: {
+    type: String,
+    required: [ true, 'Se requiere un documento para esta accion' ]
+  },
+  company: {
+    type: String,
+    required: [ true, 'Se requiere una empresa para esta accion' ]
+  },
+  specialty: String,
+  rol: String,
+  url: String,
+  brand: String,
+  model: String,
+  flighttime: {
+    type: String,
+    required: [ true, 'Se requiere una cantidad de despegues y aterrizajes para esta accion' ]
+  },
+  takeoffs: {
+    type: String,
+    required: [ true, 'Se requiere una cantidad de despegues y aterrizajes para esta accion' ]
+  },
+  type: String,
+  pilot: { 
+    type: Schema.Types.ObjectId, 
+    ref: 'Pilot', 
+    require: true
+  }
+},
+{
+  timestamps: true,
+});
+
+const FlightLog = model('FlightLogs',flightlogSchema)
+module.exports = FlightLog
